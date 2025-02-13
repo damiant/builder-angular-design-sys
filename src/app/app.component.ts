910c443b6bac09ba1a62098bff3699ee26f6d08e
Damian Tarnawsky <damian@builder.io>
2025-02-13 09:55:26 -0800

    feat: integrate builder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LayoutComponent],
+  imports: [RouterOutlet, LayoutComponent, MatButton],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
